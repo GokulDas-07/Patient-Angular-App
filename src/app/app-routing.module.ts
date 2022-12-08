0467d3901ce6548b8bacb8fb1469f4998f3db543
Gokul Das M <Gokul@NTP-LAP-723.chn.nestgroup.net>
2022-12-08 15:29:35 +0530

    search

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AddPatientComponent } from './add-patient/add-patient.component';
+import { SearchPatientComponent } from './search-patient/search-patient.component';
 import { ViewPatientComponent } from './view-patient/view-patient.component';
 
 const routes: Routes = [
   {path:"",component:AddPatientComponent},
-  {path:"view",component:ViewPatientComponent}
+  {path:"view",component:ViewPatientComponent},
+  {path:"search",component:SearchPatientComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,14 +7,16 @@ import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { AddPatientComponent } from './add-patient/add-patient.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { ViewPatientComponent } from './view-patient/view-patient.component'
+import { ViewPatientComponent } from './view-patient/view-patient.component';
+import { SearchPatientComponent } from './search-patient/search-patient.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     AddPatientComponent,
     NavbarComponent,
-    ViewPatientComponent
+    ViewPatientComponent,
+    SearchPatientComponent
   ],
   imports: [
     BrowserModule,
